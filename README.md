6214fbef9dbadb3f4543ae18b6fb9144f9f3f8ac
helq <linuxero789@gmail.com>
2021-12-21 10:16:40 -0500

    Basic operation estimation for four SNN architectures

---
+++ README.md
@@ -0,0 +1,7 @@
+# doryta-experiments
+
+This repo is da place to save the important experiments performed with/in [doryta][]. Some
+of them contain code to reproduce them; some files contain no way to recreate them
+automatically because they require some user manipulation.
+
+[doryta]: https://github.com/helq/doryta
---
+++ gol/average-of-1000-randinit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ROOT_DORYTA=..
+MODELS_DIR="$ROOT_DORYTA"/data/models
+TOOLS_DIR="$ROOT_DORYTA"/tools
+
+mkdir -p gol-random/output
+
+tar -xf $MODELS_DIR/gol/gol-random.tar.xz -C gol-random
+
+for file in gol-random/gol-random/*.bin; do
+  plain_name=${file%.bin}
+  src/doryta --gol-model --load-spikes=$file --spike-driven \
+    --output-dir=gol-random/output/${plain_name##*/} --extramem=10000 \
+    --probe-stats --end=100.5 #--probe-firing --probe-firing-buffer=40000
+done
+
+python "$TOOLS_DIR"/general/aggregate_stats.py --path 'gol-random/output/gol-random-*' \
+  --save gol-random/
+python "$TOOLS_DIR"/general/total_stats.py --path gol-random --groups '[400,400]' \
+  --iterations $((200 * 1000)) --csv 'gol-random/gol_100_iters_on_1000_random'
